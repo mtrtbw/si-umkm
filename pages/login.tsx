98ea8cefabcaa6a19eb5197a5a32e47e17a648bb
M tartib Wicaksana <muhtartibw@gmail.com>
2025-06-24 16:14:43 +0700

    update login

--- pages/login.tsx
+++ pages/login.tsx
@@ -72,7 +72,7 @@ export default function LoginPage() {
 
         {/* Form Login */}
         <div className="md:w-1/2 p-8 bg-white text-black">
-          <h2 className="text-2xl font-bold text-center text-yellow-600 mb-6">
+          <h2 className="text-2xl font-bold text-center text-yellow-600 mb-10">
             Masuk ke Akun Admin
           </h2>
 
@@ -110,27 +110,6 @@ export default function LoginPage() {
               Masuk
             </button>
           </div>
-
-          {/* Divider */}
-          <div className="flex items-center my-5">
-            <div className="flex-grow border-t border-gray-300" />
-            <span className="px-2 text-gray-400 text-sm">atau</span>
-            <div className="flex-grow border-t border-gray-300" />
-          </div>
-
-          {/* Google Login */}
-          <button
-            onClick={loginWithGoogle}
-            className="w-full border border-gray-300 py-2 rounded-md text-gray-700 hover:bg-yellow-100 transition flex items-center justify-center gap-2"
-          >
-            <svg className="w-5 h-5" viewBox="0 0 48 48">
-              <path fill="#FFC107" d="M43.6 20.5H42V20H24v8h11.3C33.3 32.7 29.1 36 24 36c-6.6 0-12-5.4-12-12s5.4-12 12-12c3.1 0 5.9 1.2 8.1 3.1l6-6C34.2 5.5 29.4 3 24 3 12.4 3 3 12.4 3 24s9.4 21 21 21c11 0 20-9 20-20 0-1.3-.1-2.1-.4-3.5z"/>
-              <path fill="#FF3D00" d="M6.3 14.7l6.6 4.8C14 16.2 18.6 13 24 13c3.1 0 5.9 1.2 8.1 3.1l6-6C34.2 5.5 29.4 3 24 3c-7.5 0-14 4.1-17.7 10.3z"/>
-              <path fill="#4CAF50" d="M24 45c5.1 0 9.9-1.9 13.5-5.1l-6.3-5.2C29 36.8 26.6 38 24 38c-5.1 0-9.4-3.2-11-7.7l-6.6 5.1C10.1 41 16.5 45 24 45z"/>
-              <path fill="#1976D2" d="M43.6 20.5H42V20H24v8h11.3c-1.4 3.9-5.2 7-9.3 7-5.1 0-9.4-3.2-11-7.7l-6.6 5.1C10.1 41 16.5 45 24 45c11 0 20-9 20-20 0-1.3-.1-2.1-.4-3.5z"/>
-            </svg>
-            Masuk dengan Google
-          </button>
         </div>
       </div>
     </div>
